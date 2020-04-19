aaa19c2b174a3c54fb2913bcea3d0931abba2394
Paulo R. Pereira de Souza <paulorpereirasouza@hotmail.com>
2020-04-19 13:33:57 -0300

    #316

--- src/Nfce/Aiapps.Nfce.Tests/NfceApiTest.cs
+++ src/Nfce/Aiapps.Nfce.Tests/NfceApiTest.cs
@@ -1,6 +1,7 @@
 using Aiapps.Nfce.Api;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using System;
 using System.Threading.Tasks;
 
 namespace Aiapps.Nfce.Tests
@@ -8,11 +9,6 @@ namespace Aiapps.Nfce.Tests
     [TestClass]
     public class NfceApiTest
     {
-        private static Credencial ValidCredencial = new Credencial
-        {
-            Email = "teste@aiapps.com.br",
-            Senha = "123456"
-        };
         [TestMethod]
         public async Task EmitirAsync_Credencial_Invalida_Test()
         {
@@ -30,7 +26,7 @@ namespace Aiapps.Nfce.Tests
         }
 
         [TestMethod]
-        public async Task EmitirAsync_Credencial_Valida_Test()
+        public async Task EmitirAsync_Credencial_Valida_Conflito_Test()
         {
             var nfceApi = new NfceApi(ValidCredencial);
             var response = await nfceApi.EmitirAsync(new Pedido
@@ -54,11 +50,36 @@ namespace Aiapps.Nfce.Tests
                 },
                 Desconto = 0,
             });
-            Assert.AreEqual("Requisição inválida", response.Erro);
+            Assert.AreEqual("Pedido 6 já foi enviado", response.Erro);
         }
 
-        private string response = @"{'numero':null,'serie':null,'chaveAcesso':null,'situacao':null,'cliente':null,'valorTotal':10.0,'itens':[],'url':null,'sefaz':{'emitidoEm':'2020-01-01T00:00:00','protocolo':null,'url':null,'motivo':'Forma de pagamento é necessário.Se não tiver opções cadastre uma forma de pagamento em configurações.','codigoStatus':null}}";
-
+        [TestMethod]
+        public async Task EmitirAsync_Credencial_Valida_Test()
+        {
+            var nfceApi = new NfceApi(ValidCredencial);
+            var response = await nfceApi.EmitirAsync(new Pedido
+            {
+                Referencia = Guid.NewGuid().ToString(),
+                Itens = new Item[] {
+                    new Item {
+                        Cfop = "5.102",
+                        NCM = "2203.00.00",
+                        ProdutoId = "1",
+                        ProdutoNome = "Cerveja",
+                        Quantidade = 1,
+                        ValorUnitario = 5m
+                    }
+                },
+                Pagamentos = new Pagamento[] {
+                    new Pagamento {
+                        Tipo = "01",
+                        Valor = 5
+                    }
+                },
+                Desconto = 0,
+            });
+            Assert.IsNotNull(response.ChaveAcesso);
+        }
 
         [TestMethod]
         public void Parse_Test()
@@ -82,5 +103,13 @@ namespace Aiapps.Nfce.Tests
             var response = await nfceApi.DanfeAsync("31200400000000000000650010000000051842021836");
             Assert.AreEqual("NF-e 1-5 nÃ£o estÃ¡ autorizada", response.ReasonPhrase);
         }
+
+        private static Credencial ValidCredencial = new Credencial
+        {
+            Email = "teste@aiapps.com.br",
+            Senha = "123456"
+        };
+
+        private string response = @"{'numero':null,'serie':null,'chaveAcesso':null,'situacao':null,'cliente':null,'valorTotal':10.0,'itens':[],'url':null,'sefaz':{'emitidoEm':'2020-01-01T00:00:00','protocolo':null,'url':null,'motivo':'Forma de pagamento é necessário.Se não tiver opções cadastre uma forma de pagamento em configurações.','codigoStatus':null}}";
     }
 }
