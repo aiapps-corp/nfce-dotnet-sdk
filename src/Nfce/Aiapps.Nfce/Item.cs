246badc38405ad46b62fb6e29f139ea58ef85fd2
Paulo R. Pereira de Souza <paulorpereirasouza@hotmail.com>
2020-12-26 18:09:05 -0300

    Composição, Customização e Desconto

--- src/Nfce/Aiapps.Nfce/Item.cs
+++ src/Nfce/Aiapps.Nfce/Item.cs
@@ -14,5 +14,16 @@ namespace Aiapps.Nfce
         public string NCM { get; set; }
         public string Cfop { get; set; }
         public bool EhTaxa { get; set; }
-    }    
+        public CustomizacaoItem[] Customizacao { get; set; } = new CustomizacaoItem[0];
+    }
+
+    public class CustomizacaoItem
+    {
+        public string ProdutoId { get; set; }
+        public string ProdutoNome { get; set; }
+        public decimal Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal Desconto { get; set; }
+
+    }
 }
--- src/Nfce/Aiapps.Nfce/Produto.cs
+++ src/Nfce/Aiapps.Nfce/Produto.cs
@@ -16,8 +16,10 @@ namespace Aiapps.Nfce
         public string Cest { get; set; }
         public string Medida { get; set; }
         public Categoria Categoria { get; set; } = new Categoria();
-
+        public Composicao[] Composicao { get; set; } = new Composicao[0];
+        public Customizacao[] Customizacao { get; set; } = new Customizacao[0];
         public decimal Valor { get; set; }
+        public decimal Desconto { get; set; }
         public string UrlImagem { get; set; }
         public bool? EhTaxaServico { get; set; }
         public bool? EhCreditoLoja { get; set; }
@@ -30,5 +32,38 @@ namespace Aiapps.Nfce
         public string Id { get; set; }
         public string Nome { get; set; }
         public string UrlImagem { get; set; }
+    }
+
+    public class Composicao
+    {
+        public Produto Produto { get; set; }
+        public bool DeveIncluirNaNota { get; set; }
+        public decimal Quantidade { get; set; }
+        public decimal Valor { get; set; }
+        public string Observacao { get; set; }
+    }
+
+    public class Customizacao
+    {
+        public string Titulo { get; set; }
+        public decimal? Minmo { get; set; }
+        public decimal? Maximo { get; set; }
+        public decimal? QuantidadeOpcoesGratuitas { get; set; }
+        public OpcaoCustomizacao[] Opcoes { get; private set; } = new OpcaoCustomizacao[0];
+        public TipoOpcao TipoOpcao { get; set; }
+    }
+    public class OpcaoCustomizacao 
+    {
+        public virtual Produto Produto { get; set; }
+        public decimal Quantidade { get; set; } = 1;
+        public string DescricaoQuantidade { get; set; }
+        public decimal? Valor { get; set; }
+        public bool PodeSerGratuito { get; set; }
+    }
+    public enum TipoOpcao
+    {
+        Adicional = 0,
+        UnicaEscolha = 1,
+        MultiplaEscolha = 2,
     }
 }
