e885db5e8ee9d2937e310098d4c295e203b92d78
Paulo R. Pereira de Souza <paulorpereirasouza@hotmail.com>
2020-12-28 13:01:01 -0300

    ItemBase

--- src/Nfce/Aiapps.Nfce/Item.cs
+++ src/Nfce/Aiapps.Nfce/Item.cs
@@ -6,18 +6,17 @@ namespace Aiapps.Nfce
 {
     public abstract class ItemBase
     {
-
         public string ProdutoId { get; set; }
         public string ProdutoNome { get; set; }
         public decimal Quantidade { get; set; }
         public decimal ValorUnitario { get; set; }
         public decimal Desconto { get; set; }
-        public string NCM { get; set; }
-        public string Cfop { get; set; }
         public string Uuid { get; set; }
     }
     public class Item : ItemBase
     {
+        public string NCM { get; set; }
+        public string Cfop { get; set; }
         public bool EhTaxa { get; set; }
         public CustomizacaoItem[] Customizacao { get; set; } = new CustomizacaoItem[0];
     }
