a866ca2fd67c46b56eaab60ab7393f5924944878
Paulo Renato Pereira de Souzaa <paulosouza@aiapps.com.br>
2021-03-01 17:52:45 -0300

    IndicadorIntermediador

--- src/Nfce/Aiapps.Nfce/Pedido.cs
+++ src/Nfce/Aiapps.Nfce/Pedido.cs
@@ -18,9 +18,28 @@ namespace Aiapps.Nfce
         public Item[] Itens { get; set; } = new Item[0];
         public Pagamento[] Pagamentos { get; set; } = new Pagamento[0];
         public EntregaPedido Entrega { get; set; } = new EntregaPedido();
+        public IndicadorIntermediador? IndicadorIntermediador { get; set; }
         public decimal Desconto { get; set; }
         public string ContaCliente { get; set; }
         public string Referencia { get; set; }
         public string Situacao { get; set; }
+    }
+
+    /// <summary>
+    /// indIntermed - Indicador de intermediador/marketplace 
+    /// 0=Operação sem intermediador(em site ou plataforma própria)
+    /// 1=Operação em site ou plataforma de terceiros(intermediadores/marketplace)
+    /// </summary>
+    public enum IndicadorIntermediador
+    {
+        /// <summary>
+        /// 0-Operação sem intermediador(em site ou plataforma própria)
+        /// </summary>
+        SemIntermediador = 0,
+
+        /// <summary>
+        /// 1-Operação em site ou plataforma de terceiros(intermediadores/marketplace)
+        /// </summary>
+        ComIntermediador = 1,
     }
 }
