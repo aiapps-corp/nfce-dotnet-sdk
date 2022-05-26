b4643781917d0e150b158e9898918ac1dfdd637c
Paulo Renato Pereira de Souzaa <paulosouza@aiapps.com.br>
2022-05-26 10:05:29 -0300

    data cancelamento

--- src/Nfce/Aiapps.Sdk.Invoices/Sefaz.cs
+++ src/Nfce/Aiapps.Sdk.Invoices/Sefaz.cs
@@ -7,6 +7,7 @@ namespace Aiapps.Sdk.Invoices
     public class Sefaz
     {
         public DateTime EmitidoEm { get; set; }
+        public DateTime? CanceladoEm { get; set; }
         public string Protocolo { get; set; }
         public string Url { get; set; }
         public string Motivo { get; set; }
