a9999982d57154c1f19fe4e3c27e09b8aa57cf96
Paulo Renato Pereira de Souzaa <paulosouza@aiapps.com.br>
2022-12-15 13:28:48 -0300

    ValorUnitario round 2

--- src/Nfce/Aiapps.Sdk.Orders/Item.cs
+++ src/Nfce/Aiapps.Sdk.Orders/Item.cs
@@ -6,12 +6,16 @@ namespace Aiapps.Sdk.Orders
 {
     public abstract class ItemBase
     {
+        private decimal _valorUnitario;
         public string ProdutoId { get; set; }
         public string ProdutoNome { get; set; }
         public decimal Quantidade { get; set; }
         public decimal? QuantidadeMovimentacaoEstoque { get; set; } = 1;
         
-        public decimal ValorUnitario { get; set; }
+        public decimal ValorUnitario {
+            get { return _valorUnitario; }
+            set { _valorUnitario = decimal.Round(value, 2); }
+        }
         public decimal Desconto { get; set; }
         public string Uuid { get; set; }
     }
